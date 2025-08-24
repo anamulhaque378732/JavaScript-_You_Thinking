cbc75ab3451af379b1a989c7bfe6b5609ec77080
Anamul Haque <anamulhaque4631@gmail.com>
2025-08-24 15:52:29 +0600

    execution

---
+++ Recursion/Recursion.js
@@ -0,0 +1,40 @@
+// recursion in js nijek nijer modde call kora
+
+
+
+
+// let myFunc = function () {
+//     myFunc();
+// }
+// console.log(myFunc);
+
+
+// 1+2+3+4+5+6+7+8+9+n
+
+// let total = 0;
+// let n = 9;
+// for (let i = 1; i <= n; i++) {
+//     total += i
+// }
+// console.log(total);
+
+// ------------------------ 2---------
+// f(n-1) + n=f(n)
+
+// function sum(n) {
+//     if (n === 0) {
+//         return 0
+//     } else {
+
+//         return sum(n - 1) + n;
+
+//     };
+// };
+// console.log(sum(50));
+
+
+var n = 10000;
+console.log((n * (n + 1)) / 2);
+
+
+
