91bc351dd83568187a7598f803d210601d2f4d17
Anamul Haque <anamulhaque4631@gmail.com>
2025-10-20 15:15:15 +0600

    Curring

---
+++ Iterator/Async.js
@@ -0,0 +1,38 @@
+// async iterator
+
+const myAsyncIterator = {
+    async *[Symbol.asyncIterator]() {
+        yield "hello";
+        yield 'async';
+        yield 'iterator'
+    }
+};
+
+(async () => {
+    for await (let x of myAsyncIterator) {
+        console.log(x);
+    };
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
