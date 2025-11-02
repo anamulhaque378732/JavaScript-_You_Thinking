484f2325a1a10dbfd9b0fecf9abfee0b28511a04
Anamul Haque <anamulhaque4631@gmail.com>
2025-11-02 23:10:53 +0600

    copy To ClipBoard

---
+++ InterviewQuestion/copy.js
@@ -0,0 +1,32 @@
+
+
+function copyToClipBoard(str) {
+
+    const element = document.createElement("textarea");
+    element.value = str;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand("copy");
+    document.body.removeChild(element);
+
+
+
+};
+
+
+
+function handleClick() {
+    const text = document.getElementById("text");
+
+    copyToClipBoard(text.innerText);
+
+
+};
+
+
+
+
+
+
+
+
---
+++ InterviewQuestion/valueSwap.js
@@ -0,0 +1,127 @@
+// 1. how can we swap values
+//[1, 2, 3, 4, 5, 6]; --> [5,2,3,4,1]
+
+const arr = [1, 2, 3, 4, 5, 6];
+
+// 1.1 temp variable
+// let temp = arr[0];
+// arr[0] = arr[4];
+// arr[4] = temp;
+//console.log(arr);
+
+
+// 1.2  array destructuring
+
+//[arr[0], arr[4]] = [arr[4], arr[0]]
+
+//console.log(arr);
+
+
+//1.3  math
+
+let a = 1;
+let b = 6;
+b = a + (a = b) - b;
+
+//console.log(a, b);
+
+
+
+
+// 2.how to copy things from clipboard
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//3.  destructuring aliases
+
+//const language = { name: "JS", founded: "1995", founder: "brendan Eich" };
+
+
+
+//4. get value as a data type
+
+// <input id="number" value="123" type="number"   />
+
+//5. remove duplicate from array
+// [,2,2,3,5,5]-->[1,2,5];
+
+
+// 6. compare two array by value
+//compare ([1,3],[1,5]) false
+
+// 7.  shuffling an array
+//[1,2,3,4,5] -->[2,1,4,3,5]
+
+
+// 8. using comma operator
+// x=(2,3) --> what is the output of x here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
