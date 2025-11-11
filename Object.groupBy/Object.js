aeabf62f454f3a22b61b094860610e7fbba9d521
Anamul Haque <anamulhaque4631@gmail.com>
2025-11-11 21:49:05 +0600

    gropup

---
+++ Object.groupBy/Object.js
@@ -0,0 +1,79 @@
+
+const languages = [
+    { title: "Javascript", type: "scripting" },
+    { title: "Java", type: "Normal" },
+    { title: "Python", type: "scripting" }
+];
+
+
+// console.log(
+//     "Languages\n",
+//     languages,
+//     "\n-----------------------------"
+
+// );
+
+let scriptingLanguages = {};
+// way ---1
+
+languages.forEach((language) => {
+    const type = language.type;
+    if (!scriptingLanguages[type]) {
+        scriptingLanguages[type] = [];
+
+    };
+    scriptingLanguages[type].push(language)
+
+
+});
+
+//console.log(scriptingLanguages);
+
+//way-2
+
+
+scriptingLanguages = languages.reduce((res, language) => {
+
+    const type = language.type;
+    if (!res[type]) {
+        res[type] = [];
+    };
+
+    res[type].push(language);
+    return res;
+}, {});
+
+//console.log(scriptingLanguages);
+
+
+
+// modern way
+
+scriptingLanguages = Object.mapBy(languages, lang => lang.type);
+console.log(scriptingLanguages);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
